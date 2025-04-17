c0885b1c5bc942dfe8d329b2faf8378ba5b99717
taeseok <tsk941229@gmail.com>
2025-04-17 21:37:35 +0900

    feat: lombok 의존성 수정

--- src/main/java/com/luckyvicky/web/admin/auth/controller/ApiAuthController.java
+++ src/main/java/com/luckyvicky/web/admin/auth/controller/ApiAuthController.java
@@ -2,6 +2,7 @@ package com.luckyvicky.web.admin.auth.controller;
 
 import com.luckyvicky.web.common.member.dto.MemberDTO;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -10,10 +11,11 @@ import java.util.HashMap;
 public class ApiAuthController {
 
     @PostMapping("/api/admin/auth/login")
-    public HashMap<String, Object> login(MemberDTO memberDTO) {
+    public HashMap<String, Object> login(@RequestBody MemberDTO memberDTO) {
 
-//        System.out.println("memberId : " + memberDTO.getMemberId());
-//        System.out.println("password : " + memberDTO.getPassword());
+
+        System.out.println(memberDTO.getMemberId());
+        System.out.println(memberDTO.getPassword());
 
         HashMap hashMap = new HashMap();
         hashMap.put("test", "ㅇㅇ");
--- src/main/java/com/luckyvicky/web/common/member/dto/MemberDTO.java
+++ src/main/java/com/luckyvicky/web/common/member/dto/MemberDTO.java
@@ -3,10 +3,9 @@ package com.luckyvicky.web.common.member.dto;
 import lombok.*;
 
 @Getter
+@Setter
 @Builder
-@NoArgsConstructor
 @AllArgsConstructor
-@RequiredArgsConstructor
 public class MemberDTO {
 
     private String memberId;
