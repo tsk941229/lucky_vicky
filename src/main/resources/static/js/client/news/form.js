965ecf0ba0f3f9a967b2da67b56687951e253c1c
taeseok <tsk941229@gmail.com>
2025-04-23 00:14:04 +0900

    feat: news 파일 작업중

--- src/main/resources/static/js/client/news/form.js
+++ src/main/resources/static/js/client/news/form.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  init();
+    init();
 });
 
 const init = () => {
@@ -8,81 +8,87 @@ const init = () => {
 
 const save = async () => {
 
-  const param = {
-    parentId: getParentId(),
-    category: getDom("category").value,
-    title: getDom("title").value,
-    content: getDom("content").value,
-    nickname: getDom("nickname").value,
-    password: getDom("password").value,
-    newsFile: getDom("newsFile").files[0]
-  }
+    const param = {
+        category: getDom("category").value,
+        title: getDom("title").value,
+        content: getDom("content").value,
+        nickname: getDom("nickname").value,
+        password: getDom("password").value
+    }
 
-  // if(!validate(param)) return;
+    // 파일 선택 했을때만 넣기
+    if(getDom("newsFile").files.length > 0) {
+        param["newsFile"] = getDom("newsFile").files[0];
+    }
 
-  const formData = objToFormData(param);
+    // parent_id (답글일 때)
+    // param["parentId"] = ??
 
-  const {status, data} = await fetchPOST("/api/client/news/save", formData);
+    // if(!validate(param)) return;
 
-  console.log("status",status);
-  console.log("data",data);
+    const formData = objToFormData(param);
 
-  if(!status) {
-    alert("등록에 실패했습니다.");
-    return;
-  }
+    const {status, data} = await fetchPOST("/api/client/news/save", formData);
 
-  alert("등록이 완료되었습니다.");
+    console.log("status", status);
+    console.log("data", data);
+
+    if (!status) {
+        alert("등록에 실패했습니다.");
+        return;
+    }
+
+    alert("등록이 완료되었습니다.");
 
 }
 
 const validate = (param) => {
 
-  if(!param.title) {
-    alert("제목을 입력해주세요.");
-    getDom("title").focus();
-    return false;
-  }
-  if(!param.nickname) {
-    alert("닉네임을 입력해주세요.");
-    getDom("nickname").focus();
-    return false;
-  }
-  if(!param.password) {
-    alert("비밀번호를 입력해주세요.");
-    getDom("password").focus();
-    return false;
-  }
+    if (!param.title) {
+        alert("제목을 입력해주세요.");
+        getDom("title").focus();
+        return false;
+    }
+    if (!param.nickname) {
+        alert("닉네임을 입력해주세요.");
+        getDom("nickname").focus();
+        return false;
+    }
+    if (!param.password) {
+        alert("비밀번호를 입력해주세요.");
+        getDom("password").focus();
+        return false;
+    }
 
-  return true;
+    return true;
 }
 
 const validateFile = (param) => {
-  console.log("param", param);
+    console.log("param", param);
 }
 
 // 답글인 경우 (parent_id)
 const getParentId = () => {
-  return null;
+    return null;
 }
 
 // 파일 하나(multiple x)를 가정
 const chooseFile = (el) => {
-  let {name, size, type} = el.files[0];
+    let {name, size, type} = el.files[0];
 
-  // Byte -> MB
-  size = (size / (1024 * 1024)).toFixed(2);
+    // Byte -> MB
+    size = (size / (1024 * 1024)).toFixed(2);
 
-  let originalName = name.split(".")[0];
-  let extension = name.split(".")[1];
+    let originalName = name.split(".")[0];
+    let extension = name.split(".")[1];
 
-  const param = {
-    originalName: originalName,
-    size: size,
-    extension: extension,
-    type: type
-  }
+    const param = {
+        originalName: originalName,
+        size: size,
+        extension: extension,
+        type: type
+    }
 
-  validateFile(param);
+    validateFile(param);
 
 }
